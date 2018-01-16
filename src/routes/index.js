0c135a5883dfeb9b627931069b94806a788a5044
xiaofengz <xiaofengz0720@gmail.com>
2018-01-17 01:22:54 +0800

    add swiper component

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Main from 'CONTAINERS/Main';
 import Editor from 'CONTAINERS/Editor'
 import EditorMarkdown from 'CONTAINERS/EditorMarkdown'
+import Content from 'CONTAINERS/Content'
 
 import {
     Router,
@@ -14,6 +15,7 @@ import {
 const routes = (
     <Router history={hashHistory}>
     <Route path="/" component={Main}>
+        <IndexRoute component={Content} />
         <Route path='writeArticle' component={Editor} />
         <Route path='markdown' component={EditorMarkdown} />
     </Route>
