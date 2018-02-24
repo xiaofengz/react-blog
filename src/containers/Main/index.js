5b7660c3e9e1cec628094684fff279e3c2da0753
xiaofengz <xiaofengz0720@gmail.com>
2018-02-24 17:37:52 +0800

    add github star

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -10,6 +10,8 @@ class Main extends Component {
         return (
             <div className="main-body" style={{minWidth:"1280px"}}>
                 <Header />
+                <iframe style={{border:"none",float:"right"}} src="https://ghbtns.com/github-btn.html?user=xiaofengz&repo=react-blog&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px">
+                </iframe>
                 {this.props.children}
             </div>
             
